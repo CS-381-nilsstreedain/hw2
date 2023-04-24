d4de9ad5d2a626a448db0dfcc0b4d64f92737346
Nils Streedain <nils@streedain.com>
2023-04-23 20:13:30 -0700

    Added sizeTree function

--- HW2.hs
+++ HW2.hs
@@ -1,3 +1,13 @@
-module HW2 where
+-- HW2.hs
+-- Author: Nils Streedain (streedan@oregonstate.edu)
+-- Date: 04/23/2023
 
+module HW2 where
 import HW2types
+import Data.List (nub)
+
+-- Problem 1: Trees
+-- sizeTree function
+sizeTree :: Tree -> Int
+sizeTree Leaf = 0
+sizeTree (Node _ l r) = 1 + sizeTree l + sizeTree r
